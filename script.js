34502488eac79d29fa66fdae6e6b1ba43360df81
Hayley McHugh <hayleym522@gmail.com>
2021-09-30 21:52:54 -0700

    more java changes

--- script.js
+++ script.js
@@ -9,16 +9,17 @@ var splitLowerLetters = lowerLetters.split("")
 var splitUpperLetters = upperLetters.split("")
 var splitLowerSymbols = symbols.split("")
 
-var all = []
-var character = []
-var password = []
-
 var passwordLength = window.prompt("Please choose a password length (min 8 characters)")
 var pickedLowerLetters = window.prompt("Confirm if you want lowercase letters (yes/no)")
 var pickedUpperLetters = window.prompt("Confirm if you want uppercase letters (yes/no)")
 var pickedNumbers = window.prompt("Confirm if you want numbers (yes/no)")
 var pickedSymbols = window.prompt("Confirm if you want symbols (yes/no)")
 
+var character = []
+var password = []
+
+
+function generatePassword () {
 if (passwordLength.length)
 for (i = 0; i < passwordLength; i++) {
   var randomCharacter = character[Math.floor(Math.random() * character.length)];
@@ -31,13 +32,13 @@ if (pickedNumbers) {
   if (pickedLowerLetters){character=character.concat(splitLowerLetters)}
   if (pickedUpperLetters){character=character.concat(splitUpperLetters)}
   if (pickedSymbols){character=character.concat(splitLowerSymbols)}
+}
 
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
-// }
+  passwordText.value = password;
+}
 
 generateBtn.addEventListener("click", writePassword);
